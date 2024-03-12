2e22a384857a970db4c2a45bd843995f7b259765
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2024-03-12 06:29:24 +0000

    data

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,12 @@
+const hero =["Batman","Spiderman"]
+
+let myobj  ={
+    name: "Ranjan",
+    age: 21
+}
+
+const myFunction = function(){
+    console.log("My name is RANJAN");
+}
+
+console.log(myobj);
