9194a893ba498cf7bcfa1cd149717c238d2eecce
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2024-03-12 08:24:25 +0000

    gyggy

---
+++ basics/date_time.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+let newDate = new Date(2024,3,10);
+//console.log(newDate.toDateString());
+
+console.log(newDate.getMonth() +1);
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
