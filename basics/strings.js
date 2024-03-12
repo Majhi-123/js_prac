5e02b9a38794d285f9c6098cc874f1f147572d6b
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2024-03-12 06:53:09 +0000

    ghgh

---
+++ basics/strings.js
@@ -0,0 +1,12 @@
+const name = "Ranjan"
+const age = 21;
+
+//string interpolation
+
+console.log(`Hello my name is ${name} and i am ${age}'s years old`);
+
+const gameName= "kumarmajhi"
+
+const another = gameName.substring(0,5);
+console.log(another);
+//.trim() remover initial and final spaces
