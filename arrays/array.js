468f506627a1417eb1b340fe4910a15e64b572ed
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2024-03-12 08:40:24 +0000

    khnrjh

---
+++ arrays/array.js
@@ -0,0 +1,18 @@
+const myArr = [0,1,3,4,'Ranjan',true]
+
+// console.log(myArr[3]);
+// myArr.push(6);
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr.includes(7));
+
+// const newArr = myArr.join()
+// console.log(newArr);
+
+const myArr1 = myArr.slice(1,3);
+console.log(myArr1);
+console.log(myArr);
+
+const myArr2 = myArr.splice(1,3);
+console.log(myArr2);
+console.log(myArr);
