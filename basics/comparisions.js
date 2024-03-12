6519e61ca9fb03de60bd10f340eeff98cd4f950e
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2024-03-12 06:29:07 +0000

    comparision

--- basics/comparisions.js
+++ basics/comparisions.js
@@ -5,3 +5,27 @@
 // console.log(null >=0);
 
 //console.log("2" ===2) //comaparision with value allongwith datatype
+
+
+// let score = "44ab"
+
+// console.log(typeof score)
+// let value = Number(score)
+// console.log(typeof value);
+// console.log(value);
+// let loggedIn = ""
+// let a = "Ranjan"
+
+// console.log(Boolean(loggedIn));
+// console.log(Boolean(a));
+//"" --> false
+//"Ranjan" --->true;
+//Number "33abc" ==> NaN
+//console.log(2**3) //2 to 3
+
+let str1 ="Ranjan"
+let str2 =" majhi"
+
+console.log(str1 + str2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
