4594e69d912363af99526d2f4d9ac75b10476724
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2024-03-12 08:06:50 +0000

    jnf

---
+++ basics/numbers.js
@@ -0,0 +1,15 @@
+const score= 100;
+//console.log(score);
+
+const balance = new Number(200);
+//console.log(balance);
+
+//Maths
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+console.log(Math.random()); //always gives b/w 0 to 1
+//how to find any number b/w min to max
+
+const min = 20;
+const max = 30;
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
