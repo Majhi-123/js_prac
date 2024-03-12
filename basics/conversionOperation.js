43156efd41ce41e57026fddfb2f8719e08c576fe
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2024-03-12 05:50:57 +0000

    conversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,15 @@
+// let score = "44ab"
+
+// console.log(typeof score)
+// let value = Number(score)
+// console.log(typeof value);
+// console.log(value);
+// let loggedIn = ""
+// let a = "Ranjan"
+
+// console.log(Boolean(loggedIn));
+// console.log(Boolean(a));
+//"" --> false
+//"Ranjan" --->true;
+//Number "33abc" ==> NaN
+
