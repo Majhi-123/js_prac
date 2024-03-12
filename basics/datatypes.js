ddac024f0e59f3da27de36646d1be33056cb8d2b
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2024-03-12 05:32:35 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,6 @@
+//alert(3 +3)   //we are node.js not browser
+
+//numbers
+//boolean
+//string ""
+//null undefined
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+console.log("Hello world");
+const accountId = 244456
+let accountEmail = "majhiranjan34@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jharsuguda"
+let accountState; 
+
+//accountId =2; can't be done
+accountEmail = "kk@gmail.com"
+accountPassword = "3456"
+accountCity = "jsg"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.log(accountId);
