53877acd1bca02f682a344fabf155e560ce72d66
Majhi-123 <95409226+Majhi-123@users.noreply.github.com>
2024-03-13 05:06:24 +0000

    loops

---
+++ iterations/loops.js
@@ -0,0 +1,96 @@
+
+// const array = ['Flash','superman','batman']
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index]
+//     console.log(element);
+// }
+
+// for(let index =1;index<=20;index++){
+//     if(index==5){
+//         console.log("Got 5");
+//         break;
+//     }
+//     console.log(`${index}`);
+// }
+
+// for(let index =1;index<=20;index++){
+//     if(index==5){
+//        // console.log("Got 5");
+//        // break;
+//        continue;
+//     }
+//     console.log(`${index}`);
+// }
+
+//while
+//let index =20;
+// while(index<=10){
+//     console.log(`value is ${index}`);
+//     index = index +2;
+// }
+// do{
+//     console.log(`value is ${index}`);
+//     index = index +2;
+// }while(index <= 10)
+
+//for of loop
+// const arr = "Ranjan Majhi";
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const mp = new Map();
+// mp.set("IN","INDIA");
+// mp.set("USA","America");
+// mp.set("PAK","PAKISTAN");
+// for (const [key,value] of mp) {
+//     console.log(value);
+// }
+
+// const myobj ={
+//     "Game1": "Nfs",
+//     "Game2" : "WWE",
+//     "Game3" : "Spiderman"
+// }
+
+//forof loop doesn't work in objects
+// for (const key in myobj) {
+//     console.log(`${key} : ${myobj[key]}`);
+// }
+
+// const program = ['cpp','java',"javascript"];
+// for (const key in program) {
+//    console.log(`${key}: ${program[key]}`);
+// }
+
+//const coding =['js','ruby','java','python','cpp'];
+
+// coding.forEach(function (item){
+//     console.log(item);
+// })
+// function call(item){
+//     console.log(item);
+// }
+// coding.forEach( (item,index)=>{
+//          console.log(item,index);
+//  })
+//coding.forEach(call);
+
+// const coding = [
+//     {
+//         language: "Javascript",
+//         file: "js"
+//     },
+//     {
+//         language: "Python",
+//         file: "Python"
+//     },
+//     {
+//         language: "C++",
+//         file: "cpp"
+//     }
+// ]
+
+// coding.forEach((item)=>{
+//     console.log(item.language);
+// })
